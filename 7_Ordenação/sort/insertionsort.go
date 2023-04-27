768657e76acec13d5ca9940bd53be264a51334c4
Pedro Rêgo <pedrorego1@hotmail.com>
2023-04-27 11:13:36 -0300

    fixing insertionsort

--- 7_Ordenação/sort/insertionsort.go
+++ 7_Ordenação/sort/insertionsort.go
@@ -1,13 +1,10 @@
 package sort
 
 func InsertionSort(v []int){
-	n := len(v)
-	for i:=1;i<n;i++{
-		valor:= v[i]
-		var j int
-		for j = i; j>0 && v[j-1] > valor;j--{
-			v[j] = v[j-1]
-		}
-		v[j] = valor
-	}
+	size := len(v)
+  for i:=1; i<size; i++{
+    for j:=i; j>0 && v[j] < v[j-1] ; j--{
+      v[j] , v[j-1] = v[j-1], v[j]
+    }
+  }
 }
