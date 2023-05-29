d56647f254363bbc1b11bb4344467ceda6f0f1a4
Pedro Rêgo <pedrorego1@hotmail.com>
2023-05-29 10:22:46 -0300

    tree update

--- 8_Árvores/main.go
+++ 8_Árvores/main.go
@@ -1,9 +1,36 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
+	"sample-app/tree"
 )
 
 func main (){
-	
+	//                9
+	//           7        12
+	//         5   8    10   13
+	//       1                   14
+	//
+	//
+	//
+	//
+	rootNode := tree.NewNode(9)
+	rootNode.Add(7)
+	rootNode.Add(12)
+	rootNode.Add(5)
+  	rootNode.Add(8)
+  	rootNode.Add(10)
+	rootNode.Add(13)
+	rootNode.Add(1)
+	rootNode.Add(14)
+
+  	rootNode = rootNode.Remove(12)
+  	rootNode = rootNode.Remove(9)
+
+	rootNode.PrintIn()
+  	//fmt.Println("Size: ", rootNode.Size())
+
+
+  	//fmt.Println("IsBst: ", nonBstNode.IsBst())
+
 }
--- 8_Árvores/tree/tree.go
+++ 8_Árvores/tree/tree.go
@@ -1,5 +1,9 @@
 package tree
 
+import (
+	"fmt"
+)
+
 type BstNode struct {
 	left  *BstNode
 	value int
@@ -54,5 +58,76 @@ func (bstNode *BstNode) Min() int{
 	} else {
 	  return bstNode.right.Max()
 	}
+}
+
+//Verificar altura (recursiva)
+func (bstNode *BstNode) Height() int {
+	hEsq := 0
+	hDir := 0
+	if bstNode.left != nil {
+	  hEsq = 1 + bstNode.left.Height()
+	}
+	if bstNode.right != nil {
+	  hDir = 1 + bstNode.right.Height()
+	}
+	if hEsq >= hDir{
+	  return hEsq
+	} else{
+	  return hDir
+	}
+}
+//Funções de navegação/impressão
+//pre ordem raiz,esq,dir
+func (bstNode *BstNode) PrintPre() {
+	fmt.Print(bstNode.value,", ")
+	if bstNode.left != nil {
+	  bstNode.left.PrintPre()
+	}
+	if bstNode.right != nil {
+	  bstNode.right.PrintPre()
+	}
   }
 
+//em ordem esq, raiz, dir
+func (bstNode *BstNode) PrintIn() {
+	if bstNode.left != nil {
+	  bstNode.left.PrintIn()
+	}
+	fmt.Print(bstNode.value,", ")
+	if bstNode.right != nil {
+	  bstNode.right.PrintIn()
+	}
+  }
+  
+  //pos ordem esq, dir, raiz
+  func (bstNode *BstNode) PrintPos() {
+	if bstNode.left != nil {
+	  bstNode.left.PrintPos()
+	}
+	if bstNode.right != nil {
+	  bstNode.right.PrintPos()
+	}
+	fmt.Print(bstNode.value,", ")
+  }
+
+  func (bstNode *BstNode) Remove(value int) *BstNode {
+	if value < bstNode.value{
+	  bstNode.left = bstNode.left.Remove(value)
+	} else if value > bstNode.value{
+	  bstNode.right = bstNode.right.Remove(value)
+	} else {   //encontramos o nó a ser removido    
+	  if bstNode.left == nil && bstNode.right == nil{          //caso 1: nó folha
+		return nil      
+	  } else if bstNode.left != nil && bstNode.right == nil {  //caso 2: nó com 1 filho (à esquerda)
+		return bstNode.left
+	  } else if bstNode.left == nil && bstNode.right != nil {  //caso 2: nó com 1 filho (à direita)
+		return bstNode.right
+	  } else {                                                  //caso 3: nó com 2 filhos
+		maxEsq := bstNode.left.Max()
+		bstNode.value = maxEsq
+		bstNode.left = bstNode.left.Remove(maxEsq)
+		return bstNode
+	  }
+	}
+	return bstNode
+  }
